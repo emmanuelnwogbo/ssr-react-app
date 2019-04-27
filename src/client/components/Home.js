721341bff210f18d7b58f7c8e4c53614fa07c805
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-04-27 22:54:19 +0100

    initial ssr setup

---
+++ src/client/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  console.log('hello')
+  return (
+    <div>
+      <div>I'm the home</div>
+      <button onClick={() => console.log('hi there')}>Press Me!</button>
+    </div>
+  );
+};
+
+export default Home;
